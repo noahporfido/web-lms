b140ef675126592b000ca3c7506804330cc7f078
Noah Porfido <noah.porfido@icloud.com>
2025-03-26 15:50:48 +0100

    center text

--- src/sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.PUBLIC_SANITY_DATASET,
-  apiVersion: "2024-12-01",
-  useCdn: false,
-});
